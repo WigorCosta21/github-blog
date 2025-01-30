7d0672daf8fbe8a2bf1028168ac6cc7e982f5fd7
Wigor <wigor21@hotmail.com>
2025-01-29 21:45:28 -0300

    style: add default theme with styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/theme/default";
+import { GlobalStyle } from "./styles/globals";
+
 const App = () => {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
       <h1>GitHub Blog</h1>
-    </>
+    </ThemeProvider>
   );
 };
 
