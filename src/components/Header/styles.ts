9238579407a3ea35b102e83e3f67b46ac3061014
Wigor <wigor21@hotmail.com>
2025-01-30 16:29:20 -0300

    feat: create header component

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+import coverImage from "../../assets/cover.png";
+
+export const HeaderContainer = styled.header`
+  width: 100%;
+  height: 296px;
+  background-image: url(${coverImage});
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
