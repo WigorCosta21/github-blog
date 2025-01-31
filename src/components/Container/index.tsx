a94a264790780967e292859dcb8dfb83d769ccc3
Wigor <wigor21@hotmail.com>
2025-01-30 21:22:37 -0300

    feat: create container component

---
+++ src/components/Container/index.tsx
@@ -0,0 +1,10 @@
+import { ReactNode } from "react";
+import * as S from "./styles";
+
+interface ContainerProps {
+  children: ReactNode;
+}
+
+export const Container = ({ children }: ContainerProps) => {
+  return <S.Container>{children}</S.Container>;
+};
---
+++ src/components/Container/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  max-width: 864px;
+  width: 100%;
+  margin: 0 auto;
+
+  @media screen and (max-width: 864px) {
+    padding: 0 1.25rem;
+  }
+`;
